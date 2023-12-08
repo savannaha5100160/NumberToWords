123813f12655e49b89eb4fd7024a3560f336705f
savannaha5100160 <5100160@mypillar7uwc.ac.za>
2023-12-08 10:19:21 +0200

    first commit

---
+++ Assignment2NumberToWords/Numbers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2NumberToWords
+{
+    public class Numbers
+
+    {
+          public static string  Numbers(int number)
+        {
+            if (number == 0)
+                return "zero";
+
+            string[] units = { "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            string[] teens = { "", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+            string[] tens = { "", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+            string result = "";
+
+            if (number / 1000 > 0)
+            {
+                result += units[number / 1000] + "Thousand";
+                number %= 1000;
+            }
+
+            if (number / 100 > 0)
+            {
+                result += units[number / 100] + "Hundred";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (result != "")
+                    result += "and";
+
+                if (number < 10)
+                    result += units[number];
+                else if (number < 20)
+                    result += teens[number - 10];
+                else
+                {
+                    result += tens[number / 10];
+                    if (number % 10 > 0)
+                        result += "" + units[number % 10];
+                }
+            }
+        }
+
+    }
+}
---
+++ Assignment2NumberToWords/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Assignment2NumberToWords
+{
+     class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a number:149");
+            Console.WriteLine("Enter a number:1149");
+            Console.WriteLine("Enter a number:42");
+            Console.WriteLine("Enter a number:890");
+
+
+            int number = Convert.ToInt32(Convert.ToInt32(Console.ReadLine());
+
+            string words = Numbers.(number);
+
+            Console.WriteLine($"In words:{words}");
+            Console.WriteLine($"one Hundred forty nine");
+            Console.WriteLine($"one thousand one Hundred forty nine");
+            Console.WriteLine($" forty  two ");
+            Console.WriteLine($"eight'Hundred  nine");
+
+
+
+        }
+    }
+}
